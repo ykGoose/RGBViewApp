52bb243b98362af5a724544c5d74961778510baa
ykGoose <yokogus@gmail.com>
2020-10-18 22:18:05 +0900

    minor change

--- RGBViewApp/ViewController.swift
+++ RGBViewApp/ViewController.swift
@@ -8,6 +8,7 @@ class ViewController: UIViewController {
     @IBOutlet var colorRedValue: UILabel!
     @IBOutlet var colorGreenValue: UILabel!
     @IBOutlet var colorBlueValue: UILabel!
+    
     @IBOutlet var colorRedSlider: UISlider!
     @IBOutlet var colorGreenSlider: UISlider!
     @IBOutlet var colorBlueSlider: UISlider!
@@ -20,21 +21,13 @@ class ViewController: UIViewController {
         colorGreenValue.text = String(format: "%.2f", colorGreenSlider.value)
         colorBlueValue.text = String(format: "%.2f", colorBlueSlider.value)
         colorView.layer.cornerRadius = 10
+        
         colorView.backgroundColor = .init(red: CGFloat(colorRedSlider.value),
                                           green: CGFloat(colorGreenSlider.value),
                                           blue: CGFloat(colorBlueSlider.value),
                                           alpha: 1)
     }
     
-    // MARK: - PublicMethod
-    func movingSlider(sliderColor: UISlider, colorLaber: UILabel) {
-        let movingResult = CGFloat(sliderColor.value)
-        colorLaber.text = String(format: "%.2f", movingResult)
-        colorView.backgroundColor = .init(red: CGFloat(colorRedSlider.value),
-                                          green: CGFloat(colorGreenSlider.value),
-                                          blue: CGFloat(colorBlueSlider.value),
-                                          alpha: 1)
-    }
     
     //MARK: - IBActions
     @IBAction func movingRedSlider() {
@@ -49,4 +42,13 @@ class ViewController: UIViewController {
         movingSlider(sliderColor: colorBlueSlider, colorLaber: colorBlueValue)
     }
 
+    //MARK: - Private Method
+    private func movingSlider(sliderColor: UISlider, colorLaber: UILabel) {
+        let movingResult = CGFloat(sliderColor.value)
+        colorLaber.text = String(format: "%.2f", movingResult)
+        colorView.backgroundColor = .init(red: CGFloat(colorRedSlider.value),
+                                          green: CGFloat(colorGreenSlider.value),
+                                          blue: CGFloat(colorBlueSlider.value),
+                                          alpha: 1)
+    }
 }
